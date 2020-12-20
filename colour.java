25337b7b257aed1fe85a164757749918e3456c5c
Aman Priyadarshi <55681932+aman-pri@users.noreply.github.com>
2020-12-20 14:34:18 +0530

    Add files via upload

---
+++ colour.java
@@ -0,0 +1,24 @@
+import javafx.scene.paint.Color;
+
+public class colour {
+    private Color c1=Color.rgb(247, 103, 37);
+    private Color c2=Color.rgb(157, 41, 122);
+    private Color c3=Color.rgb(69, 186, 204);
+    private Color c4=Color.rgb(249,200, 12);
+
+    public Color getC1() {
+        return c1;
+    }
+
+    public Color getC2() {
+        return c2;
+    }
+
+    public Color getC3() {
+        return c3;
+    }
+
+    public Color getC4() {
+        return c4;
+    }
+}
